8c0e215eeb226ed4cdd3aaed3f8ec56c4f2be049
Michael Folger <folger@zoller-usa.com>
2021-06-29 13:10:30 -0400

    updates to js, html and readme'

--- hw_README.md
+++ hw_README.md
@@ -18,16 +18,28 @@ SO THAT I can manage my time effectively
 
 ```md
 GIVEN I am using a daily planner to create a schedule
+
+<!-- DONE -->
 WHEN I open the planner
 THEN the current day is displayed at the top of the calendar
+
+<!-- DONE -->
 WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours
+
+<!-- NOT DONE -->
 WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+<!--DONE -->
 WHEN I click into a timeblock
 THEN I can enter an event
+
+<!--DONE -->
 WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
+
+<!-- NOT DONE -->
 WHEN I refresh the page
 THEN the saved events persist
 ```
--- script.js
+++ script.js
@@ -10,14 +10,120 @@ function displayTime() {
 setInterval(displayTime, 1000);
 
 
+//saving user input to local storage
 
-/*
-//set function that turns time blocks red after that time of day has passed
-if (displayTime.textContent !== document.getElementById('#timeblock8').valueOf) {
-  h4.setAttribute('class', "list-group-item-danger")
+localStorage.setItem('input','')
+
+
+function saveLocal8(){
+   var sibling = document.getElementById("save8").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+  window.localStorage.getItem('sibling')
 }
 
-//set onclick event that allows input when item is clicked
-var timeblock8El = document.getElementById('#timeblock8');
+save8.addEventListener('click',saveLocal8)
 
-timeblock8El.addEventListener('click',)
+function saveLocal9(){
+  var sibling = document.getElementById("save9").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+}
+
+save9.addEventListener('click',saveLocal9)
+
+function saveLocal10(){
+  var sibling = document.getElementById("save10").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+}
+
+save10.addEventListener('click',saveLocal10)
+
+
+function saveLocal11(){
+  var sibling = document.getElementById("save11").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+}
+
+save11.addEventListener('click',saveLocal11)
+
+
+function saveLocal12(){
+  var sibling = document.getElementById("save12").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+}
+
+save12.addEventListener('click',saveLocal12)
+
+function saveLocal1(){
+  var sibling = document.getElementById("save1").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+}
+
+save1.addEventListener('click',saveLocal1)
+
+
+function saveLocal2(){
+  var sibling = document.getElementById("save2").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+}
+
+save2.addEventListener('click',saveLocal2)
+
+
+function saveLocal3(){
+  var sibling = document.getElementById("save3").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+}
+
+save3.addEventListener('click',saveLocal3)
+
+
+function saveLocal4(){
+  var sibling = document.getElementById("save4").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+}
+
+save4.addEventListener('click',saveLocal4)
+
+
+function saveLocal5(){
+  var sibling = document.getElementById("save5").previousElementSibling.value;
+  console.log(sibling)
+  localStorage.setItem(sibling, 'value')
+}
+
+save5.addEventListener('click',saveLocal5)
+
+
+document.getElementById('save8').addEventListener('click',saveLocal8);
+document.getElementById('save9').addEventListener('click',saveLocal9);
+document.getElementById('save10').addEventListener('click',saveLocal10);
+document.getElementById('save11').addEventListener('click',saveLocal11);
+document.getElementById('save12').addEventListener('click',saveLocal12);
+document.getElementById('save1').addEventListener('click',saveLocal1);
+document.getElementById('save2').addEventListener('click',saveLocal2);
+document.getElementById('save3').addEventListener('click',saveLocal3);
+document.getElementById('save4').addEventListener('click',saveLocal4);
+document.getElementById('save5').addEventListener('click',saveLocal5);
+
+
+
+//function to set timeblocks different colors based on time i.e. red = past, green = present , yellow = future, repeat for all timeblocks
+//if (displayTime <= 8:00) {
+  //document.getElementById('timeblock8').setAttribute('class','form-control list-group-item-danger');
+  //}
+  //else if (displayTime === 8:00) {
+    //document.getElementById('timeblock8).setAtribute('class', 'form-control list-group-item-success');
+  //}
+
+  //else {
+    //document.getElementById('timeblock8).setAttribute('class','form-control list-group-item-warning');
+  //}
